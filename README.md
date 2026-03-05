35c1bb9ffa2c22ace7ffb250d3de929a07f3406b
Kamil Widzyk <kamilwidzyk@student.agh.edu.pl>
2026-03-05 12:34:23 +0100

    Dodanie parametru punktu wejściowego

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Skrypt antlr.bat
 ```
-antlr.bat plik_gramatyki.g4 string_wejsciowy
+antlr.bat plik_gramatyki.g4 punkt_startowy string_wejsciowy
 ```
 - Wymagana zainstalowana java
 - Wyświetlone zostanie drzewo i zapisane do ant_out\nazwa\nazwa.tree
--- antlr.bat
+++ antlr.bat
@@ -2,21 +2,27 @@
 setlocal EnableDelayedExpansion
 
 REM ---- sprawdzanie obecności parametrów ----
-if "%~1"=="" (
+if "%~2" == "" (
     echo.
-    echo Blad: Nie podano pliku gramatyki.
+    echo Blad: Nie podano wystarczajacej liczby parametrow.
     echo.
-    echo Uzycie: %~nx0 plik_gramatyki.g4 [tekst do parsowania]
+    echo Uzycie: %~nx0 ^<plik_gramatyki.g4^> ^<punkt_startowy^> [tekst do parsowania]
     echo.
-    echo Przyklad: %~nx0 MojaGramatyka.g4 1 + 2 * 3
+    echo Przyklad: %~nx0 Kalkulator.g4 expression 2 + 2
     echo.
-    echo Skrypt wygeneruje parser, skompiluje go i zapisze drzewo 
-    echo parsowania w katalogu ant_out\MojaGramatyka
+    echo Opis:
+    echo   - plik_gramatyki.g4: Sciezka do pliku .g4
+    echo   - punkt_startowy: Nazwa reguly w gramatyce, od ktorej zaczyna sie parsowanie
+    echo   - tekst do parsowania: Wszystkie kolejne parametry zostana polaczone w wejscie
     exit /b 1
 )
 
 REM ---- parametry ----
-set G4FILE=%1
+set "G4FILE=%~1"
+set "START_RULE=%~2"
+
+REM Przesuwamy o dwa (plik i punkt startowy), aby reszta to byl INPUT
+shift
 shift
 
 REM Zbieramy pozostałe parametry do zmiennej INPUT
@@ -42,7 +48,7 @@ copy "%G4FILE%" "%OUTDIR%\" >nul
 
 pushd "%OUTDIR%"
 
-REM ---- generowanie parsera (..\..\ bo wychodzimy z ant_out\nazwa) ----
+REM ---- generowanie parsera ----
 java -jar ..\..\antlr-4.13.2-complete.jar %NAME%.g4
 
 REM ---- kompilacja ----
@@ -51,11 +57,11 @@ javac -cp ".;..\..\antlr-4.13.2-complete.jar" *.java
 REM ---- zapis wejścia ----
 echo %INPUT% > input.txt
 
-REM ---- uruchomienie parsera ----
-java -cp ".;..\..\antlr-4.13.2-complete.jar" org.antlr.v4.gui.TestRig %NAME% r -tree < input.txt > %NAME%.tree
+REM ---- uruchomienie parsera (uzycie zmiennej %START_RULE%) ----
+java -cp ".;..\..\antlr-4.13.2-complete.jar" org.antlr.v4.gui.TestRig %NAME% %START_RULE% -tree < input.txt > %NAME%.tree
 
 echo.
-echo ====== DRZEWO PARSOWANIA ======
+echo ====== DRZEWO PARSOWANIA (Start: %START_RULE%) ======
 type %NAME%.tree
 echo ===============================
 
